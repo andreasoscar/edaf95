5cb50480624b7db6a1d6f4b776f58e50ab7e9713
andreasoscar <seolmi@hotmail.se>
2023-03-19 19:33:58 +0100

    fixed class1

--- class1/sumsq.hs
+++ class1/sumsq.hs
@@ -1,5 +1,8 @@
 sumsqrec 0 = 0
 sumsqrec n = n*n + sumsqrec (n-1)
 
-sumsqmap n = map (\x -> x*x) [x | x <- [1..5]]
-main = print (sumsqrec 5)
+sumsqmap n = sum $ map (\x -> x*x) [x | x <- [1..5]]
+
+sumsqalternative n = sum $ zipWith (*) [1..n] [1..n]
+
+main = print (sumsqmap 5)
